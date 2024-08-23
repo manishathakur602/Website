818ab246cb66b3c8a7eb5e5f610aeabed904f385
smritikumri15 <smritiss9638@gmail.com>
2024-08-23 14:43:50 +0530

    push code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import Testimonials from "./assets/Testimonials"
+import Buttons from "./Buttons"
+import Carousel from "./Carousel"
 import ContactUS from "./ContactUs"
+import Courses from "./Courses"
+import Graphic from "./Graphic"
+import Title from "./Title"
+import WebDesign from "./WebDesign"
+import WebPage from "./WebPage"
+import Word from "./Word"
 
 
 function App() {
@@ -12,6 +20,7 @@ function App() {
 
        <ContactUS/> 
     </div>
+    
     </>
   )
 }
