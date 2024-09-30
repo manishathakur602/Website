b244f9ba0f35bef0d1bfc67a7ad8a09af3037e56
Smriti Kumari <smritiss9638.com>
2024-09-30 12:27:21 +0530

    up to date

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import Testimonials from "./assets/Testimonials"
-import Buttons from "./Buttons"
+
 import Carousel from "./Carousel"
 import ContactUS from "./ContactUs"
 import Courses from "./Courses"
-import Graphic from "./Graphic"
 import Title from "./Title"
-import WebDesign from "./WebDesign"
-import WebPage from "./WebPage"
-import Word from "./Word"
+
+import Page1 from "./Page1"
+
 
 
 function App() {
@@ -16,13 +15,13 @@ function App() {
     <>
     
        <Testimonials/> 
-       <div className="bg-[#FFE28A]  ">
-       <div className="bg-black px-8 flex justify-center items-center">
-        <span className="text-blue-400">My Name Is Whatever</span>
-       </div>
+  
 
+
+<Page1 />
        <ContactUS/> 
-    </div>
+    {/* <Carousel/> */}
+    
     
     </>
   )
